d4b8ed7db9e063796b4145620e8af8ee3747310a
ruzny <roony0782045336@gmail.com>
2024-01-03 19:23:57 +0530

    middleware file was commented out

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,31 @@
-import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
+// import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
+// import { NextResponse } from "next/server";
 
-export default withAuth(
-  function middleware(request: NextRequestWithAuth) {
-    console.log(request.nextUrl);
-    console.log(request.nextauth);
+// export default withAuth(
+//   function middleware(request: NextRequestWithAuth) {
 
-    if (
-      request.nextUrl.pathname.startsWith("/products/new") &&
-      request.nextauth.token?.role !== "admin"
-    ) {
-      return NextResponse.error();
-    }
+    // if (
+    //   request.nextUrl.pathname.startsWith("/products/new") &&
+    //   request.nextauth.token?.role !== "admin"
+    // ) {
+    //   return NextResponse.error();
+    // }
 
-    // Restricted /cart path for admin just coz no cart functionality is implemented for admin
-    if (
-      request.nextUrl.pathname.startsWith("/cart") &&
-      request.nextauth.token?.role === "admin"
-    ) {
-      return NextResponse.error();
-    }
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
-  }
-);
+//     // Restricted /cart path for admin just coz no cart functionality is implemented for admin
+//     if (
+//       request.nextUrl.pathname.startsWith("/cart") &&
+//       request.nextauth.token?.role === "admin"
+//     ) {
+//       return NextResponse.error();
+//     }
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ token }) => !!token,
+//     },
+//   }
+// );
 
-export const config = { matcher: ["/products/new"] };
+// export const config = { matcher: ["/products/new"] };
+
+export default function() {}
